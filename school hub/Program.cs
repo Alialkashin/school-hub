d5bea97708022fc3b1b39cc86150754fba7902be
Alialkashin <118594530+Alialkashin@users.noreply.github.com>
2025-05-19 00:41:22 -0700

    update .gitignore

--- school hub/Program.cs
+++ school hub/Program.cs
@@ -36,7 +36,6 @@ app.MapControllerRoute(
         name: "default",
     pattern: "{controller=Home}/{action=/Index}/{id?}"
     );
-
 app.MapControllerRoute(
     name: "areas",
     pattern: "{area:exists}/{controller=Home}/{action=/Index}/{id?}");
