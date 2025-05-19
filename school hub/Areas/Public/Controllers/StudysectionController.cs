dcc7ef12712a4d5004cd05a4a4fda9205f164730
amalek <am9176661@gmail.com>
2025-05-19 10:51:12 +0300

    build studysection controller

---
+++ school hub/Areas/Public/Controllers/StudysectionController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using school_hub.Data;
+
+namespace school_hub.Areas.Public.Controllers
+{
+    public class StudysectionController : Controller
+    {
+        private readonly AppDBContext _context;
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
