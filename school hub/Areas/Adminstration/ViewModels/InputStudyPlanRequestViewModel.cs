1751f3ed3211aaab168f9201e823a2e8e6b05d38
Malek18Jassar <am9176661@gmail.com>
2025-06-30 09:38:52 +0300

    test

---
+++ school hub/Areas/Adminstration/ViewModels/InputStudyPlanRequestViewModel.cs
@@ -0,0 +1,17 @@
+﻿using school_hub.Models;
+
+namespace school_hub.Areas.Adminstration.ViewModels
+{
+    public class InputStudyPlanRequestViewModel
+    {
+        public int RequestNumber { get; set; }
+        public string StudentName { get; set; }
+        public string StudyPlanName { get; set; }
+        public int StudentId { get; set; }
+        public string StudyPlanId { get;  set; }
+
+
+    }
+
+
+}
---
+++ school hub/ViewModels/StudySectionDetailsViewModel.cs
@@ -0,0 +1,11 @@
+﻿using school_hub.Models;
+
+namespace school_hub.ViewModels
+{
+    public class StudySectionDetailsViewModel
+    {
+        public StudySection Section { get; set; }
+        public int? StudentId { get; set; }
+        public List<short> SubscribedPlanIds { get; set; } = new List<short>();
+    }
+}
